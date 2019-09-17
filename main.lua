ec9d4346d510d6f1b121df25e48b85c2b953667d
Mark Moran <markmoran@gmail.com>
2019-09-16 21:20:12 -1000

    Recursive placement outside of window

--- main.lua
+++ main.lua
@@ -1,42 +1,53 @@
 function love.load()
-    gridXCount = 20
-    gridYCount = 10
-    cellSize = 30
+    gridXCount = 20 -- how many cells across
+    gridYCount = 15 -- how many cells up and down
+    cellSize = 30 -- the size of a cell
 
-    love.window.setMode(gridXCount * cellSize, gridYCount * cellSize)
+    love.window.setMode(gridXCount * cellSize, gridYCount * cellSize) -- set the window size
 
-    snakeSegments = { {x = 3, y = 1},
+    snakeSegments = { {x = 3, y = 1}, -- last segment
                       {x = 2, y = 1},
-                      {x = 1, y = 1} }
+                      {x = 1, y = 1} } -- first (head) segment
 
-    timer = 0
-    direction = 'right'
+    timer = 0 -- time to movement
+    direction = 'right' -- starting direction
 end
 
 function love.update(dt)
 
-    timer = timer + dt
-    local timerLimit = 0.15
+    timer = timer + dt -- adds the delta time to timer
+    local timerLimit = 0.15 --
     if timer >= timerLimit then
         timer = timer - timerLimit
 
-        local nextXPosition = snakeSegments[1].x
-        local nextYPosition = snakeSegments[1].y
+        local nextXPosition = snakeSegments[1].x -- adjusting the next X
+        local nextYPosition = snakeSegments[1].y -- adjusting the next Y
 
         if direction == 'right' then
             nextXPosition = nextXPosition + 1
+            if nextXPosition > gridXCount then
+                nextXPosition = 1
+            end
         elseif direction == 'left' then
             nextXPosition = nextXPosition - 1
+            if nextXPosition < 1 then
+                nextXPosition = gridXCount
+            end
         elseif direction == 'down' then
             nextYPosition = nextYPosition + 1
+            if nextYPosition > gridYCount then
+                nextYPosition = 1
+            end
         elseif direction == 'up' then
             nextYPosition = nextYPosition - 1
+            if nextYPosition < 1 then
+                nextYPosition = gridYCount
+            end
         end
 
         table.insert(snakeSegments, 1, {x = nextXPosition, y = nextYPosition})
         table.remove(snakeSegments)
     end
-
 
 end
 
@@ -44,6 +55,10 @@ function love.draw()
 
     love.graphics.setColor(.28, .28, .28)
     love.graphics.rectangle('fill', 0, 0, gridXCount * cellSize, gridYCount * cellSize)
+
+    love.graphics.setColor(1,1,1)
+    love.graphics.print(tostring(math.ceil(snakeSegments[1].x)) , 100, 100)
+    love.graphics.print(tostring(math.ceil(snakeSegments[1].y)) , 150, 100)
 
     for segmentIndex, segment in ipairs(snakeSegments) do
         love.graphics.setColor(.6, 1, .32)
